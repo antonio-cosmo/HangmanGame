853e5054fb5fad2a997b2a38bfc9e6d377286143
Antônio Cosmo <antoniocosmosilvaneto@gmail.com>
2023-03-26 17:24:09 -0300

    modified word list

--- Program.cs
+++ Program.cs
@@ -12,11 +12,40 @@ public class Program
                             "laranja", "limao", "lima",
                             "melancia", "mamao", "melao"
                           };
+        string[] animal = { "abelha", "aguia", "aranha","arara",
+                            "avestruz","barata","baleia","bode",
+                            "burro","camelo","cachorro","cavalo",
+                            "coelho","elefante","escorpiao","foca",
+                            "formiga", "falcao","galinha","gorila",
+                            "jacare","javali","lagosta","leao","macaco",
+                            "mariposa","morcego","mosca","ovelha","peru",
+                            "porco","polvo","rato","raposa","tartaruga",
+                            "tatu","tigre","touro","urubu" };
+        
+        string[][] wordType= { fruits, animal };
 
         var random = new Random();
-        var index = random.Next(fruits.Length);
-        string randomWord = fruits[index];
-        Console.WriteLine("A palavra secreta é uma fruta!");
+        int wordTypeIndex = random.Next(wordType.Length);
+        int wordIndex;
+        string randomWord = "";
+
+        switch (wordTypeIndex)
+        {
+            case 0:
+                wordIndex = random.Next(wordType[wordTypeIndex].Length);
+                randomWord = wordType[wordTypeIndex][wordIndex];
+                Console.WriteLine($"A palavra secreta é uma fruta com {randomWord.Length} letras!");
+                break;
+            case 1:
+                wordIndex = random.Next(wordType[wordTypeIndex].Length);
+                randomWord = wordType[wordTypeIndex][wordIndex];
+                Console.WriteLine($"A palavra secreta é um animal com {randomWord.Length} letras!");
+                break;
+            default: break;
+        }
+        
+        //randomWord = fruits[random.Next(fruits.Length)];
+        //Console.WriteLine($"A palavra secreta é uma fruta com {randomWord.Length} letras!");
 
         var hangman = new Hangman(randomWord);
         hangman.Start();
